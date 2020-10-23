04cd472bf59fef0140590839a2b9d25d97e0c9aa
adwin nugroho <adwinnugroho16@gmail.com>
2020-10-23 15:20:42 +0700

    still raw gonna fix and clean up later

--- get_list_diseases.py
+++ get_list_diseases.py
@@ -4,11 +4,12 @@ from bs4 import  BeautifulSoup
 
 def get_content_per_page():
     string_url = "https://idnmedis.com/daftar-penyakit-a-z"
+    data = []
     for page in range(2, 28):
-        print()
-        print("-----------------")
-        print(string_url)
-        print("-----------------")
+        # print()
+        # print("-----------------")
+        # print(string_url)
+        # print("-----------------")
         URL = string_url
         get_url = requests.get(URL)
         content_page = BeautifulSoup(get_url.content, 'html.parser')
@@ -17,7 +18,5 @@ def get_content_per_page():
         if len(job_elements) == 0:
             print("tidak ada nama penyakit") 
         for element in job_elements:
-            print(element.text)
-    return job_elements
-
-get_content_per_page()
+            data.append(element.text)
+    return data
---
+++ get_result_search.py
@@ -0,0 +1,25 @@
+# import library
+import requests
+from bs4 import  BeautifulSoup
+import get_list_diseases
+
+data = []
+for get_data in get_list_diseases.get_content_per_page():
+    data.append(get_data)
+
+for query in data:
+    query = query.replace(' ', '+')
+    url = 'https://www.google.com/search?q={}&oq={}&aqs=chrome..69i57.5180025j0j1&sourceid=chrome&ie=UTF-8'.format(query, query) #url
+    print(url)
+    print()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}#headers
+    source=requests.get(url, headers=headers).text # url source
+    soup = BeautifulSoup(source, 'html.parser')
+    search_div = soup.find_all(class_='PZPZlf') # find all divs tha contains search result
+    for result in search_div: # loop result list
+        f = open("result.txt", "a")
+        f.write(result.text)
+        f.close()
+    #print('Title: %s'%result.h3.string) #geting h3 
+    #print('Url: %s'%result.a.get('href')) #geting a.href 
+    #print('Description: %s'%result.find(class_='st').text)
